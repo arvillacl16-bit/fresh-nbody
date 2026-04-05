b20ec9f5e503af77d62bba1bbaa620f5a94ee031
Aneesh Abbineni <aabbineni@all.health>
2026-04-05 12:18:57 -0400

    Removed warning

--- unit-tests/leapfrog-basic.cpp
+++ unit-tests/leapfrog-basic.cpp
@@ -1,6 +1,5 @@
 #include "integrator.hpp"
 #include "particle.hpp"
-#include <iostream>
 #include <gtest/gtest.h>
 
 constexpr double square(double r) { return r * r; }
@@ -10,7 +9,7 @@ TEST(Logic, LeapfrogBasic) {
   ParticleStore sim;
 
   Vec3 correct_pos{149597870, 0, 0};
-  sim.add_particles({{.mu = 1.327e11, .id = 0, .pos = {0, 0, 0}, .vel = {0, 0, 0}}, {.mu = 398600, .id = 1, .pos = correct_pos, .vel = {0, 29.78, 0}}});
+  sim.add_particles({{.pos = {0, 0, 0}, .vel = {0, 0, 0}, .mu = 1.327e30, .id = 0}, {.pos = correct_pos, .vel = {0, 29.78, 0}, .mu = 398600, .id = 1}});
 
   Leapfrog integ;
   constexpr double step_size = 365. * 86400. / 40.;
