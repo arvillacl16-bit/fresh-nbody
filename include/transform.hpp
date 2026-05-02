4c8301615ab6e52ed810e8391acd2a35d8fa68ef
Aneesh Abbineni <aabbineni@all.health>
2026-05-02 11:45:17 -0400

    Added the transformation header files

---
+++ include/transform.hpp
@@ -0,0 +1,31 @@
+#ifndef TRANSFORM_HPP_
+#define TRANSFORM_HPP_
+
+#include "particle.hpp"
+
+namespace fnb::transform {
+  void inertial_to_jacobi_pv(const ParticleStore& particles, ParticleStore& p_j);
+  void inertial_to_jacobi_pva(const ParticleStore& particles, ParticleStore& p_j);
+  void inertial_to_jacobi_a(const ParticleStore& particles, ParticleStore& p_j);
+
+  void jacobi_to_inertial_pv(ParticleStore& particles, const ParticleStore& p_j);
+  void jacobi_to_inertial_p(ParticleStore& particles, const ParticleStore& p_j);
+  void jacobi_to_inertial_a(ParticleStore& particles, const ParticleStore& p_j);
+
+  void inertial_to_demhelio_pv(const ParticleStore& particles, ParticleStore& p_h);
+  void demhelio_to_inertial_p(ParticleStore& particles, const ParticleStore& p_h);
+  void demhelio_to_inertial_pv(ParticleStore& particles, const ParticleStore& p_h);
+
+  void inertial_to_whds_pv(const ParticleStore& particles, ParticleStore& p_h);
+  void whds_to_inertial_p(ParticleStore& particles, const ParticleStore& p_h);
+  void whds_to_inertial_pv(ParticleStore& particles, const ParticleStore& p_h);
+
+  void inertial_to_barycentric_pv(const ParticleStore& particles, ParticleStore& p_b);
+  void inertial_to_barycentric_a(const ParticleStore& particles, ParticleStore& p_b);
+  void barycentric_to_inertial_p(ParticleStore& particles, const ParticleStore& p_b);
+  void barycentric_to_inertial_pv(ParticleStore& particles, const ParticleStore& p_b);
+  void barycentric_to_inertial_a(ParticleStore& particles, const ParticleStore& p_b);
+}
+
+#endif
+
