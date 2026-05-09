2b1ba9cd46f828c50beda1ade086b94187638ef0
Aneesh Abbineni <aabbineni@all.health>
2026-05-09 19:45:25 -0400

    Added Jacobi to Inertial

--- src/transform.cpp
+++ src/transform.cpp
@@ -76,4 +76,64 @@ namespace fnb::transform {
       p_j.accelerations[i] = particles.accelerations[i] - act_com_acc;
     }
   }
+
+  void jacobi_to_inertial_a(ParticleStore& particles, const ParticleStore& p_j) {
+    const size_t N = particles.N();
+    const size_t N_thres = particles.N();
+    double M = 0;
+#pragma omp parallel for reduction(+:M)
+    for (size_t i = 0; i < N; ++i) M += p_j.mus[i];
+    Vec3 R = p_j.accelerations[0] * M;
+    for (size_t i = N; i > N_thres; --i) particles.accelerations[i] += R;
+    for (size_t i = N_thres; i > 1; --i) {
+      R = (R + p_j.mus[i] * p_j.accelerations[i]) / M;
+      particles.accelerations[i] += R;
+      M -= p_j.mus[i];
+      R *= M;
+    }
+
+    particles.accelerations[0] = R / p_j.mus[0];
+  }
+
+  void jacobi_to_inertial_p(ParticleStore& particles, const ParticleStore& p_j) {
+    const size_t N = particles.N();
+    const size_t N_thres = particles.N();
+    double M = 0;
+#pragma omp parallel for reduction(+:M)
+    for (size_t i = 0; i < N; ++i) M += p_j.mus[i];
+    Vec3 R = p_j.positions[0] * M;
+    for (size_t i = N; i > N_thres; --i) particles.positions[i] += R;
+    for (size_t i = N_thres; i > 1; --i) {
+      R = (R + p_j.mus[i] * p_j.positions[i]) / M;
+      particles.positions[i] += R;
+      M -= p_j.mus[i];
+      R *= M;
+    }
+    particles.positions[0] = R / p_j.mus[0];
+  }
+
+  void jacobi_to_inertial_pv(ParticleStore& particles, const ParticleStore& p_j) {
+    const size_t N = particles.N();
+    const size_t N_thres = particles.N();
+    double M = 0;
+#pragma omp parallel for reduction(+:M)
+    for (size_t i = 0; i < N; ++i) M += p_j.mus[i];
+    Vec3 R_pos = p_j.positions[0] * M;
+    Vec3 R_vel = p_j.velocities[0] * M;
+    for (size_t i = N - 1; i > N_thres; --i) {
+      particles.positions[i] -= R_pos;
+      particles.velocities[i] -= R_vel;
+    }
+    for (size_t i = N_thres; i > 1; --i) {
+      R_pos = (R_pos + p_j.mus[i] * p_j.positions[i]) / M;
+      R_vel = (R_vel + p_j.mus[i] * p_j.velocities[i]) / M;
+      particles.positions[i] += R_pos;
+      particles.velocities[i] += R_vel;
+      M -= p_j.mus[i];
+      R_pos *= M;
+      R_vel *= M;
+    }
+    particles.positions[0] = R_pos / p_j.mus[0];
+    particles.velocities[0] = R_vel / p_j.mus[0];
+  }
 }
