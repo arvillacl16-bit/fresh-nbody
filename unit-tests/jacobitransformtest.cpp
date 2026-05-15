da7e3d69838e9983bd022fdd347142f50b50fc4f
Aneesh Abbineni <aabbineni@all.health>
2026-05-14 21:17:44 -0400

    Added jacobi test, it sigtrapped

---
+++ unit-tests/jacobitransformtest.cpp
@@ -0,0 +1,47 @@
+#include <gtest/gtest.h>
+#include <ranges>
+#include <random>
+#include "particle.hpp"
+#include "transform.hpp"
+
+namespace rv = std::views;
+
+TEST(JacobiTransform, ReversibilityTest) {
+  using namespace fnb;
+
+  std::mt19937 gen(std::random_device{}());
+  std::uniform_real_distribution<> dist(-20, 20);
+
+  auto gen_random_particle = [&](unsigned long long id) {
+    return IndParticle {
+      .pos = {dist(gen) * 1.496e8, dist(gen) * 1.496e8, dist(gen) * 1.496e8},
+      .vel = {dist(gen), dist(gen), dist(gen)},
+      .mu = 398600,
+      .id = id,
+    };
+  };
+
+  ParticleStore p;
+  ParticleStore useless;
+  ParticleStore orig;
+
+  p.add_particle({.mu = 1.327e11, .id = 0});
+
+  for (auto k : rv::iota(1, 7)) {
+    p.add_particle(gen_random_particle(k));
+    useless.add_particle(IndParticle{});
+  }
+
+  orig = p;
+  
+
+  transform::inertial_to_jacobi_pv(p, useless);
+  transform::jacobi_to_inertial_pv(p, useless);
+
+  Vec3 total_error;
+  for (auto i : rv::iota(0, 7)) {
+    total_error += p[i].pos() - orig[i].pos();
+  }
+
+  ASSERT_LE(total_error.mag2(), 100.);
+}
