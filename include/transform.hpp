251d5d685435a78b928dffe1dad75473535dc3f5
Aneesh Abbineni <aabbineni@all.health>
2026-05-16 08:58:31 -0400

    Finished transform API

--- include/transform.hpp
+++ include/transform.hpp
@@ -7,16 +7,21 @@ namespace fnb::transform {
   void inertial_to_jacobi_pos(const ParticleStore& particles, ParticleStore& p_j);
   void inertial_to_jacobi_vel(const ParticleStore& particles, ParticleStore& p_j);
   void inertial_to_jacobi_acc(const ParticleStore& particles, ParticleStore& p_j);
-
   void jacobi_to_inertial_pos(ParticleStore& particles, const ParticleStore& p_j);
   void jacobi_to_inertial_vel(ParticleStore& particles, const ParticleStore& p_j);
   void jacobi_to_inertial_acc(ParticleStore& particles, const ParticleStore& p_j);
 
   void inertial_to_demhelio_pos(const ParticleStore& particles, ParticleStore& p_h);
+  void inertial_to_demhelio_vel(const ParticleStore& particles, ParticleStore& p_h);
+  void inertial_to_demhelio_acc(const ParticleStore& particles, ParticleStore& p_h);
+  void demhelio_to_inertial_pos(ParticleStore& particles, const ParticleStore& p_h);
   void demhelio_to_inertial_vel(ParticleStore& particles, const ParticleStore& p_h);
   void demhelio_to_inertial_acc(ParticleStore& particles, const ParticleStore& p_h);
 
   void inertial_to_whds_pos(const ParticleStore& particles, ParticleStore& p_h);
+  void inertial_to_whds_vel(const ParticleStore& particles, ParticleStore& p_h);
+  void inertial_to_whds_acc(const ParticleStore& particles, ParticleStore& p_h);
+  void whds_to_inertial_pos(ParticleStore& particles, const ParticleStore& p_h);
   void whds_to_inertial_vel(ParticleStore& particles, const ParticleStore& p_h);
   void whds_to_inertial_acc(ParticleStore& particles, const ParticleStore& p_h);
 
